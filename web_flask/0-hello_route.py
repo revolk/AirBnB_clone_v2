eb409ba4df0a33bb57473d0983d686bb99254cf6
revolk <finalrevolk@gmail.com>
2024-06-12 14:30:54 -0700

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,11 @@
-#!/usr/bin/python3
-'''A simple Flask web application.
-'''
+#!/usr/bin/env python3
 from flask import Flask
 
-
 app = Flask(__name__)
-'''The Flask application instance.'''
-app.url_map.strict_slashes = False
 
+@app.route('/airbnb-onepage/')
+def hello_hbnb():
+    return "Hello HBNB!"
 
-@app.route('/')
-def index():
-    '''The home page.'''
-    return 'Hello HBNB!'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
